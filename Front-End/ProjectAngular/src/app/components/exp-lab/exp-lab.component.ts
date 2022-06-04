86eb3bcf47bf824bc3de060b078c71d35f27342e
Nicolas D. Rodriguez <nikoo853@gmail.com>
2022-06-04 20:25:19 -0300

    ajustes en componentes de angular

--- Front-End/ProjectAngular/src/app/components/exp-lab/exp-lab.component.ts
+++ Front-End/ProjectAngular/src/app/components/exp-lab/exp-lab.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-exp-lab',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exp-lab.component.css']
 })
 export class ExpLabComponent implements OnInit {
+  expList: any;
 
-  constructor() { }
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe( data => {
+      this.expList=data.experiencia;
+  })
   }
-
 }
--- Front-End/ProjectAngular/src/app/components/habilidades/habilidades.component.ts
+++ Front-End/ProjectAngular/src/app/components/habilidades/habilidades.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-habilidades',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./habilidades.component.css']
 })
 export class HabilidadesComponent implements OnInit {
+  skillList:any;
 
-  constructor() { }
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe( data => {
+      this.skillList=data.skill;
+    })
   }
 
 }
