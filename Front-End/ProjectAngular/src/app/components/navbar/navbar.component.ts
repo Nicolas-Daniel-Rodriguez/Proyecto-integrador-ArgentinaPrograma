de2c765dcf339758557839b7c5808302967518ad
Nicolas D. Rodriguez <nikoo853@gmail.com>
2022-08-09 21:09:03 -0300

    boton logout

--- Front-End/ProjectAngular/src/app/components/navbar/navbar.component.ts
+++ Front-End/ProjectAngular/src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { personaService } from 'src/app/servicios/persona.service';
 import { Persona } from '../../interfaces/persona/persona.component';
 import { HttpErrorResponse } from '@angular/common/http';
+import { TokenService } from 'src/app/servicios/token.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,19 +10,25 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  isLogged = false;
   public personas!: Persona[];
   roles!: string[];
   isAdmin: boolean = false;
   modalSwitch:boolean = false;
   bbdd:any;
 
-  constructor(private personaService:personaService) { }
+  constructor(private personaService:personaService, private tokenService: TokenService) { }
 
   ngOnInit(): void {
     this.getPersonas();
     this.personaService.$modal.subscribe((valor) => {this.modalSwitch= valor})
     this.personaService.getPersonas().subscribe( data => {
       this.bbdd=data;})
+      if(this.tokenService.getToken()){
+        this.isLogged=true;
+      }else{
+        this.isLogged = false;
+      }
   }
 
   public getPersonas(): void {
@@ -39,4 +46,8 @@ export class NavbarComponent implements OnInit {
     this.modalSwitch = true;
   }
 
+  onLogout():void{
+    this.tokenService.logOut();
+    window.location.reload();
+  }
 }
--- Front-End/ProjectAngular/src/app/login/login.component.ts
+++ Front-End/ProjectAngular/src/app/login/login.component.ts
@@ -49,7 +49,7 @@ export class LoginComponent implements OnInit {
         this.tokenService.setUserName(data.nombreUsuario);
         this.tokenService.setAuthorities(data.authorities);
         this.roles = data.authorities;
-        this.router.navigate(['/Inicio']);
+        window.location.reload();
       },
       error: (err) => {
         this.isLogged = false;
