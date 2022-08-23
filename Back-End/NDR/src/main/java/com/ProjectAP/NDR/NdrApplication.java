f3a462eae55903fc7ebbbd98225cbc6d363299fe
Nicolas D. Rodriguez <nikoo853@gmail.com>
2022-08-22 21:04:34 -0300

    classmain link modified

--- Back-End/NDR/src/main/java/com/ProjectAP/NDR/NdrApplication.java
+++ Back-End/NDR/src/main/java/com/ProjectAP/NDR/NdrApplication.java
@@ -19,7 +19,7 @@ public class NdrApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-                    corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+                    corsConfiguration.setAllowedOrigins(Arrays.asList("https://intense-woodland-84122.herokuapp.com/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
